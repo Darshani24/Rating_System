21761f38e2b1f946b9b9e89d8974e79c6829b853
unknown <radkumari@std.appsc.sab.ac.lk>
2025-03-31 17:49:44 +0530

    Added Files

---
+++ Rating_System/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rating from './components/Rating'
+
+const App = () => {
+  return (
+    <div className='font-kanit'>
+      <Rating/>
+    </div>
+    
+  )
+}
+
+export default App
