be08d6cabfa89042deb14b89def64547d6797afe
Darshani <160106465+Darshani24@users.noreply.github.com>
2025-05-04 15:17:19 +0530

    Add files via upload
    Added Rating Stars

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Rating from './components/Rating'
+
+const App = () => {
+  return (
+    <div className='font-kanit bg-amber-600'>
+    
+      <Rating/>
+    </div>
+    
+  )
+}
+
+export default App
